d5bb03630815e88b3d0a1cfb57efa658cbebf155
Matthew Lau <mattlau7890@gmail.com>
2024-08-05 15:11:09 -0700

    Fixed bug

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Pantry Management",
+  title: "Pantry Management App",
   description: "A small Pantry Management App",
 };
 
